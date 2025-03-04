3a76d057e3bbf7f39becf079875e841770f45afc
Emine Oraklabel <emine.oraklabel@softwire.com>
2025-03-04 10:54:48 +0000

    Refactored NextFiveArrivalGetter

--- src/NextFiveArrivalGetter.cs
+++ src/NextFiveArrivalGetter.cs
@@ -6,13 +6,7 @@ namespace BusBoardApp
         {
             List<ArrivingBus> allArrivingBuses = await TflClient.GetStopPointArrivals(stopCode);
             allArrivingBuses.Sort((x, y) => x.TimeToStation.CompareTo(y.TimeToStation));
-            List<ArrivingBus> nextFiveBusList = new List<ArrivingBus>{};
-
-            foreach (ArrivingBus arrivingBus in allArrivingBuses.Take(5))
-            {
-                nextFiveBusList.Add(new ArrivingBus(arrivingBus.LineId, arrivingBus.TimeToStation));
-            }
-            return nextFiveBusList;
+            return allArrivingBuses.Take(5).ToList();
         }
     }
 }
